5a1ab25d1e7f7caea3e75913c4fb8e0d797d7068
tonyjie <jiajieli0429@gmail.com>
2025-04-25 00:26:34 -0400

    update script

--- technology_mapping/run_abc.sh
+++ technology_mapping/run_abc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -x
-net="c17_2"
-K=3
-# abc -c "read dataset/iscas85/input/${net}.blif; if -K ${K}; write_blif dataset/iscas85/output/${net}_lut${K}_abc.blif"
+net="c432"
+K=6
+# iscas85 directory
+abc -c "read dataset/iscas85/input/${net}.blif; if -K ${K}; write_blif dataset/iscas85/output/${net}_lut${K}_abc.blif"
+abc -c "cec dataset/iscas85/input/${net}.blif dataset/iscas85/output/${net}_lut${K}_abc.blif"
 
 # demo directory
-abc -c "read dataset/demo/input/${net}.blif; if -K ${K}; write_blif dataset/demo/output/${net}_lut${K}_abc.blif"
-
-abc -c "cec dataset/demo/input/c17.blif dataset/demo/output/c17_lut3_abc.blif"
+# abc -c "read dataset/demo/input/${net}.blif; if -K ${K}; write_blif dataset/demo/output/${net}_lut${K}_abc.blif"
+# abc -c "cec dataset/demo/input/c17.blif dataset/demo/output/c17_lut3_abc.blif"
--- technology_mapping/run_baseline.sh
+++ technology_mapping/run_baseline.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -x
-net="c17_2"
-K=3
-# python program/main.py --net dataset/iscas85/input/${net}.blif --k ${K} --optimize size --out dataset/iscas85/output/${net}_lut${K}_baseline.blif 
+net="c432"
+K=6
+# iscas85 directory
+python program/main.py --net dataset/iscas85/input/${net}.blif --k ${K} --optimize size --out dataset/iscas85/output/${net}_lut${K}_baseline.blif
+abc -c "cec dataset/iscas85/input/${net}.blif dataset/iscas85/output/${net}_lut${K}_baseline.blif"
+
 
 # demo directory
-python program/main.py --net dataset/demo/input/${net}.blif --k ${K} --optimize size --out dataset/demo/output/${net}_lut${K}_baseline.blif 
-
-abc -c "cec dataset/demo/input/${net}.blif dataset/demo/output/${net}_lut${K}_baseline.blif"
+# python program/main.py --net dataset/demo/input/${net}.blif --k ${K} --optimize size --out dataset/demo/output/${net}_lut${K}_baseline.blif 
+# abc -c "cec dataset/demo/input/${net}.blif dataset/demo/output/${net}_lut${K}_baseline.blif"
