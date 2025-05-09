c8ba48b47b075cf69b3164c519e09a290d1d6a2a
chhzh123 <hc676@cornell.edu>
2025-05-09 01:59:00 +0000

    Remove collect_results

--- collect_results.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# python ./scripts/collect_results.py ./llm_solutions/20250508_171954 protein_sequence_design
-python ./scripts/collect_results.py ./llm_solutions/20250508_182449 protein_sequence_design
