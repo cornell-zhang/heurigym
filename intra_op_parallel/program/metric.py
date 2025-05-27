e50501fe759b0f33ffdd561757ff8dc27793c439
chhzh123 <hc676@cornell.edu>
2025-05-27 04:29:27 -0400

    Fix dataset names and metrics

---
+++ intra_op_parallel/program/metric.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def normalize_score_iopddl(score, baseline):
+    if np.isnan(score) or np.isnan(baseline):
+        raise ValueError("Invalid score or baseline")
+    if baseline == 0:
+        baseline = 1 + 1e-6
+    if score == 0:
+        score = 1 + 1e-6
+    return min(1, np.log10(baseline) / np.log10(score))
--- protein_sequence_design/program/metric.py
+++ protein_sequence_design/program/metric.py
@@ -1,2 +1,2 @@
 def normalize_score(score, baseline):
-    return min(1, score / baseline)
+    return min(1, (100 - baseline) / (100 - score))
